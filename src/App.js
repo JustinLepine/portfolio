c23d91ca1d612d206770f7dcd5ccf1fe5df18a93
Justin Lepine <lepine.justin@yahoo.com>
2022-04-20 11:24:08 -0400

    scroll activated nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import Nav from './components/Nav/Nav';
-import Hero from './components/Hero/Hero';
-import './App.scss';
+import Nav from "./components/Nav/Nav";
+import Hero from "./components/Hero/Hero";
+import "./App.scss";
 
 function App() {
+  const body = document.body;
+  let lastScroll = 0;
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+      body.classList.remove("scroll-up");
+      return;
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+      body.classList.remove("scroll-up");
+      body.classList.add("scroll-down");
+    } else if (
+      currentScroll < lastScroll &&
+      body.classList.contains("scroll-down")
+    ) {
+      body.classList.remove("scroll-down");
+      body.classList.add("scroll-up");
+    }
+    lastScroll = currentScroll;
+  });
+
   return (
     <div className="App">
       <Nav />
