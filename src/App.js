de04a0f40669871788f00bea6e2f8c9c362dfb21
Justin Lepine <lepine.justin@yahoo.com>
2022-04-20 11:33:00 -0400

    fixed BEM naming

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ function App() {
   window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
     if (currentScroll <= 0) {
-      body.classList.remove("scroll-up");
+      body.classList.remove("app__scroll-up");
       return;
     }
 
-    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-      body.classList.remove("scroll-up");
-      body.classList.add("scroll-down");
+    if (currentScroll > lastScroll && !body.classList.contains("app__scroll-down")) {
+      body.classList.remove("app__scroll-up");
+      body.classList.add("app__scroll-down");
     } else if (
       currentScroll < lastScroll &&
-      body.classList.contains("scroll-down")
+      body.classList.contains("app__scroll-down")
     ) {
-      body.classList.remove("scroll-down");
-      body.classList.add("scroll-up");
+      body.classList.remove("app__scroll-down");
+      body.classList.add("app__scroll-up");
     }
     lastScroll = currentScroll;
   });
 
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
       <Hero />
     </div>
