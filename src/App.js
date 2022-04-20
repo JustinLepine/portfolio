7709df7605a47516835b706610cd7f4ce602c2fe
Justin Lepine <lepine.justin@yahoo.com>
2022-04-20 10:33:27 -0400

    hero layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Nav from './components/Nav/Nav'
+import Nav from './components/Nav/Nav';
+import Hero from './components/Hero/Hero';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.scss';
+
+function Hero() {
+  return (
+    <section className='hero'>
+        <h1 className='hero__title'>Justin Lepine</h1>
+        <h2 className='hero__subtitle'>Full Stack Web Developer</h2>
+    </section>
+  )
+}
+
+export default Hero
