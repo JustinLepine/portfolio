62cc77a633ee59c5bc8033a9096e4415d8780da2
Justin Lepine <lepine.justin@yahoo.com>
2022-05-17 10:57:58 -0400

    added smooth scroll for nav tabs

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,20 @@
 import React from 'react';
-// import Images from '../../assets/images';
 import './Nav.scss';
 
 function Nav() {
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
   return (
     <nav className='nav'>
-        {/* <img className='nav__logo' src={Images.curly}/> */}
         <h2 className='nav__title'>JL</h2>
         <ul className='nav__list'>
             <li className='nav__links'><a href='#hero'>Home</a></li>
