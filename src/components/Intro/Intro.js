b6f9f10bdc0127d6ecebdab4eb33e4c22c2b6c29
Justin Lepine <lepine.justin@yahoo.com>
2022-05-17 15:41:11 -0400

    general restyling

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,8 +4,8 @@ import './Intro.scss';
 function Intro() {
   return (
     <section className='intro'>
-        <h2 className='intro__title'>About Me:</h2>
-        <p className='intro__content'>With a passion for tech in 2022 I made the decision to change my life and pursue a lifetime dream of coding</p>
+        <h2 className='intro__title'>About Me :</h2>
+        <p className='intro__content'>With a passion for tech in 2022 I made the decision to change my life and pursue a lifetime dream of coding.</p>
     </section>
   )
 }
