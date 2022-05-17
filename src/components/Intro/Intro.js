43553dc05afb3f6a7c32546b4d0c6783f5524eaa
Justin Lepine <lepine.justin@yahoo.com>
2022-05-17 16:53:06 -0400

    added faded in and out for intro section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Intro.scss';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
-function Intro() {
+function Intro() {  
+
+  const { ref, inView } = useInView({
+    threshold: 0.4
+  });
+  
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if(inView) {
+      animation.start({
+        transition: { duration: 0.8 },
+        x:0,
+        opacity: 1
+      });
+    }
+    if(!inView) {
+      animation.start({
+        transition: { duration: 1 },
+        x: 0,
+        opacity: 0
+      })
+
+    }
+  }, [inView, animation]);
+  
+
   return (
-    <section className='intro'>
+<section className='intro' ref={ref}>
+    <motion.div className='intro__box' animate={ animation }>
         <h2 className='intro__title'>About Me :</h2>
         <p className='intro__content'>With a passion for tech in 2022 I made the decision to change my life and pursue a lifetime dream of coding.</p>
-    </section>
+    </motion.div>
+</section>
   )
 }
 
