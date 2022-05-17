49ceb2e24f707c11563a12b3c2a6562733ffeed6
Justin Lepine <lepine.justin@yahoo.com>
2022-05-17 10:16:04 -0400

    3d cube added to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,34 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import "./Hero.scss";
 import Typewriter from "./Typewriter";
+import HeroCube from "./HeroCube";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function Hero() {
-
   useEffect(() => {
-  const typewriter = new Typewriter(document.body.querySelector('.type'))
+    const typewriter = new Typewriter(document.body.querySelector(".hero__type"));
 
-  typewriter
-    .pauseFor(1500)
-    .typeString("Hey!")
-    .pauseFor(800)
-    .deleteChars(4)
-    .typeString("Hello!")
-    .pauseFor(1000)
-    .typeString(' I\'m Justin Lepine a Web Developer from Montreal, Canada!')
-    .pauseFor(500)
-    .deleteChars(1)
-    .pauseFor(500)
-    .typeString('🍁')
-    .pauseFor(2000)
-    .typeString(' : )')
-    .start();
-    console.log('typewriter')
-},[])
+    typewriter
+      .pauseFor(1500)
+      .typeString("Hey!")
+      .pauseFor(800)
+      .deleteChars(4)
+      .typeString("Hello!")
+      .pauseFor(1000)
+      .typeString("\n I'm Justin Lepine a Web Developer from Montreal, Canada!")
+      .pauseFor(500)
+      .deleteChars(1)
+      .pauseFor(500)
+      .typeString("🍁")
+      .pauseFor(2000)
+      .typeString(" : )")
+      .start();
+    console.log("typewriter");
+  }, []);
 
   return (
-    <section id='hero' className="hero">
-      <div id="type" className="type"></div>
-      <h2 className="hero__subtitle">Full Stack Web Developer</h2>
+    <section id="hero" className="hero">
+      <Canvas className="hero__canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <HeroCube />
+        </Suspense>
+      </Canvas>
+      <div id="type" className="hero__type"></div>
     </section>
   );
 }
