56365ec467f484811c511625e99d7646813986d2
Justin Lepine <lepine.justin@yahoo.com>
2022-06-13 15:05:08 -0400

    removed nav section

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,29 +1,12 @@
-import React, { useState } from 'react';
-import Dropdown from "../Dropdown/Dropdown";
-import Drop from "../../assets/svg/drop.svg";
+import React from 'react';
+
 import './Nav.scss';
 
 function Nav() {
-  const [showDrop, setShowDrop] = useState(false);
-
-  const onClick = () => {
-    setShowDrop(!showDrop);
-  };
   
   return (
     <nav className='nav'>
-      <a a href='#hero' className='nav__title'>JL</a>
-      <div className='nav__right'>
-        <ul className='nav__list'>
-            <li className='nav__links'><a href='#hero'>Home</a></li>
-            <li className='nav__links'><a href='#projects'>Projects</a></li>
-            <li className='nav__links'><a href='#skills'>Skills</a></li>
-        </ul>
-        <div className="nav__drop" onClick={onClick}>
-          <img src={Drop} alt="drop"></img>
-          {showDrop === true ? <Dropdown /> : ''}
-        </div>
-      </div>
+
     </nav>
   )
 }
