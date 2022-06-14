5798ce885b006741da9f442e17c4a0795ff50ace
Justin Lepine <lepine.justin@yahoo.com>
2022-06-13 20:04:39 -0400

    added react-scroll and added Linking

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import { Link } from "react-scroll"
 import './Nav.scss';
 
 function Nav() {
   
   return (
     <nav className='nav'>
-
+      <Link to="hero" smooth={true} duration={0} delay={0}>
+        <p>JL</p>
+      </Link>
+      <div>
+        <Link className='nav__links' to="projects" smooth={true} duration={0} delay={0}>
+          Projects
+        </Link>
+        <Link className='nav__links' to="skills" smooth={true} duration={0} delay={0}>
+          Skills
+        </Link>
+      </div>
     </nav>
   )
 }
