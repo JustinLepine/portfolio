5a36a03ea45d5fd8be9fcf12c8eefb088cf93ac6
Justin Lepine <lepine.justin@yahoo.com>
2022-08-31 18:01:59 -0400

    fixed nav to projects

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Github from "../../assets/svg/github.svg";
 import ArrowBack from "../../assets/svg/arrow-back.svg";
 import ArrowForward from "../../assets/svg/arrow-forward.svg";
-import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 import "./Slider.scss";
 
 const Slider = (props) => {
   const data = props.ProjectLists;
   const [current, setCurrent] = useState(0);
   const length = data.length;
+
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        transition: { duration: 0.8 },
+        x: 0,
+        opacity: 1,
+      });
+    }
+    if (!inView) {
+      animation.start({
+        transition: { duration: 1 },
+        x: 0,
+        opacity: 0,
+      });
+    }
+  }, [inView, animation]);
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
@@ -23,9 +47,9 @@ const Slider = (props) => {
   }
 
   return (
-    <>
+  <motion.section animate={animation} ref={ref} id='projects'>
     <h1 className="slider__title">Projects :</h1>
-    <section className="slider">
+    <div className="slider">
       <img src={ArrowBack} alt="arrow-back" onClick={prevSlide} className="slider__arrow" />
       {data.map((project, index) => {
           return (
@@ -42,7 +66,9 @@ const Slider = (props) => {
                   <p className="slider__desc">{project.desc}</p>
                   <div>
                     <p>{project.tech}</p>
-                    <img className="slider__github" src={Github} alt="github" />
+                    <a target='_blank' rel="noreferrer" href={project.github}>
+                      <img className="slider__github" src={Github} alt="github" />
+                    </a>
                   </div>
                 </div>
                 <img src={project.image} alt="preview" className="slider__img" />
@@ -52,8 +78,8 @@ const Slider = (props) => {
         );
     })}
       <img src={ArrowForward} alt="arrow" onClick={nextSlide} className="slider__arrow" />
-    </section>
-        </>
+    </div>
+  </motion.section>
   );
 };
 
