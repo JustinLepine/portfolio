8c604e5ca0ad02b0aec9a778998c5cae7713e36e
Justin Lepine <lepine.justin@yahoo.com>
2024-01-25 22:12:16 -0500

    sized nav bar, changed font

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Nav = () => {
   return (
     <div className='nav'>
-      <h1 className='nav_name'>JL</h1>
+      <h1 className='nav_name'>Justin</h1>
+      <div className='nav_right'>
+        <p>PROJECTS</p>
+        <p>ABOUT</p>
+        <p>CONTACT</p>
+      </div>
     </div>
   )
 }
